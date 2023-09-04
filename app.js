ed7071fc883a62ac7c35a1dc1749f1d26237fc2b
Jimmy Clairy <jimcl@hotmail.fr>
2023-09-04 18:16:58 +0200

    End projet password

--- app.js
+++ app.js
@@ -5,11 +5,12 @@
 
 function getRandomNumber(min, max) {
     let randomNumber = crypto.getRandomValues(new Uint32Array(1))[0]
+
     // 32 bits = max de 0 à 4 294 967 295
     randomNumber = randomNumber / 4294967296
+
     return Math.trunc(randomNumber * (max - min + 1)) + min;
 }
-console.log(getRandomNumber(0, 5));
 
 function addASet(fromCode, toCode) {
     let characterList = "";
@@ -25,8 +26,71 @@ const charactersSet = {
     numbers: addASet(48, 57),
     symbols: addASet(33, 47) + addASet(58, 64) + addASet(91, 96) + addASet(123, 126)
 }
-let numbersList = addASet(48, 57)
-let lowerCaseCharsList = addASet(97, 122)
-let upperCaseCharsList = addASet(65, 90)
-let symbolsList = addASet(48, 57)
-console.log(charactersSet);
+
+const copyBtn = document.querySelector(".copy-btn")
+copyBtn.addEventListener('click', handlePasswordCopy)
+let locked = false
+function handlePasswordCopy() {
+    const password = document.querySelector('.password-content').textContent
+    navigator.clipboard.writeText(password)
+
+    if (!locked) {
+        copyBtn.classList.add("active")
+        locked = true;
+        setTimeout(() => {
+            copyBtn.classList.remove("active")
+            locked = false;
+        }, 600)
+    }
+}
+
+const checkboxes = document.querySelectorAll('.checkbox-group input')
+const rangeInput = document.querySelector('.range-group input')
+const rangeLabel = document.querySelector('.range-group label')
+updateRangeLabel()
+
+function updateRangeLabel() {
+    rangeLabel.textContent = `Taille du mot de passe : ${rangeInput.value}`
+}
+
+const passwordSettings = {
+    length: rangeInput.value,
+    ifLowerCaseChars: checkboxes[0].checked,
+    ifUpperCaseChars: checkboxes[1].checked,
+    ifNumbers: checkboxes[2].checked,
+    ifSymbols: checkboxes[3].checked,
+}
+
+rangeInput.addEventListener('change', handleInputChange)
+function handleInputChange() {
+    updateRangeLabel()
+    passwordSettings.length = rangeInput.value
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('change', handleCheckboxChange))
+function handleCheckboxChange(e) {
+    if (e.target.id === 'lowerCaseChars') { passwordSettings.ifLowerCaseChars = e.target.checked }
+    if (e.target.id === 'upperCaseChars') { passwordSettings.ifUpperCaseChars = e.target.checked }
+    if (e.target.id === 'numbers') { passwordSettings.ifNumbers = e.target.checked }
+    if (e.target.id === 'symbols') { passwordSettings.ifSymbols = e.target.checked }
+}
+
+const btnGenerate = document.querySelector('.generate-password-btn')
+btnGenerate.addEventListener('click', handleGenerate)
+
+function handleGenerate() {
+    const password = `${passwordSettings.ifLowerCaseChars ? charactersSet.lowerCaseChars : ''}${passwordSettings.ifUpperCaseChars ? charactersSet.upperCaseChars : ''}${passwordSettings.ifNumbers ? charactersSet.numbers : ''}${passwordSettings.ifSymbols ? charactersSet.symbols : ''}`
+
+    if (password === '') {
+        alert('Choisi des options')
+        return
+    }
+
+    let passwordGenerate = ''
+    for (let index = 0; index < passwordSettings.length; index++) {
+        passwordGenerate += password[getRandomNumber(0, password.length - 1)]
+    }
+    const passwordText = document.querySelector('.password-content')
+    passwordText.textContent = passwordGenerate
+}
+handleGenerate()
